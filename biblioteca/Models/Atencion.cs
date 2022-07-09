c28d228868b5ccdee863be5b1215b6abc4d90bb6
PowerCisco <francisco.castillo@outlook.com>
2022-07-09 10:46:49 -0400

    Modelos actualizados
    Los modelos para Autor y Libro fueron eliminados.
    Los modelos para Paciente, Atencion y Medico agregados.
    Falta el/los modelo(s) para el inventario de medicamentos.

---
+++ biblioteca/Models/Atencion.cs
@@ -0,0 +1,14 @@
+namespace biblioteca.Models;
+
+
+public class Atencion{
+    public int AtencionId {get;set;}
+    public string? Diagnostico {get;set;}
+    public string? Tratamiento {get;set;}
+    public DateTime FechaAtencion {get;set;}
+    public string? MedicamentosIndicados {get;set;}
+
+    public Paciente? Paciente {get;set;}
+    public Medico? Medico {get;set;}
+ 
+}
---
+++ biblioteca/Models/Medico.cs
@@ -0,0 +1,11 @@
+namespace biblioteca.Models;
+
+
+public class Medico{
+    public int MedicoId {get;set;}
+    public string? PNombre {get;set;}
+    public string? SNombre {get;set;}
+    public string? PApellido {get;set;}
+    public string? SApellido {get;set;}
+    public string? Especialidad {get;set;}
+}
